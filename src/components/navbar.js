25674af5245cc9e26ebbfde0925118cd6b209e12
Shawn Roy <shawnr2324@gmail.com>
2023-10-10 11:34:41 -0400

    Remove dark mode

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,7 +16,7 @@ function NavLink({to,children}){
 function MobileNav({open, setOpen}){
     return (
         <div className={`absolute top-0 left-0 h-screen w-screen bg-gray-900 dark:bg-gray-900 transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
-            <div className="flex items-center justify-center filter drop-shadow-md bg-white dark:bg-gray-900 h-20"> {/*logo container*/}
+            <div className="flex items-center justify-center filter drop-shadow-md bg-gray-900 dark:bg-gray-900 h-20"> {/*logo container*/}
                 <Link className="text-xl font-semibold text-white dark:text-white" href="/">Our Bar</Link>
             </div>
             <div className="flex flex-col ml-4">
